29a7e6cd2c330c43afe123b40bbc780d7373e840
fuad.sadigov <fuad.sadigov@aih.az>
2024-04-26 16:40:53 +0400

    add some changhe

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         setLoading(true);
         setResults(prevState => [...prevState, { message: message, response: false }]);
         setMessage('')
-    
+
         try {
             const encryptedMessage = CryptoJS.AES.encrypt(JSON.stringify({ message }), import.meta.env.VITE_SECRET_KEY).toString();
             const requestOptions = {
@@ -30,18 +30,18 @@ function App() {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ message: message })
             };
-    
+
             const response = await fetch('https://chatbot-psi-nine.vercel.app/message', requestOptions);
             const resultJson = await response.json();
             // const bytes = CryptoJS.AES.decrypt(resultJson.message, import.meta.env.VITE_SECRET_KEY);
             // const decryptedResponse = bytes.toString(CryptoJS.enc.Utf8);
-    
+
             setResults(prevState => {
                 const newState = [...prevState];
                 newState[newState.length - 1] = { message: message, response: resultJson.message };
                 return newState;
             });
-    
+
             setResponse(resultJson.message);
             setLoading(false);
         } catch (error) {
@@ -67,7 +67,7 @@ function App() {
                                     <div>{result.message}</div>
                                 </div>
                                 <div className='response'>
-                                    <img width={38} height={35} src="https://chatbot-psi-nine.vercel.app/public/sefa.jpg" alt="" />
+                                    <img width={38} style={{ objectFit: 'cover' }} height={35} src="https://wcu.edu.az/uploads/images/img_60e6b8ef05077.jpg" alt="" />
                                     {result.response != false ? <div className='result-text' dangerouslySetInnerHTML={{ __html: result.response }} /> : <div className='load-avatar'></div>}
                                 </div>
                             </div>
@@ -78,9 +78,9 @@ function App() {
                 <form onSubmit={sendMessage} className='form'>
                     <input className='input' placeholder='Write something' value={message}
                         onChange={(e) => setMessage(e.target.value)} />
-                        {message && (
+                    {/* {message && (
                             <img onClick={sendMessage} className='send-btn' src="https://chatbot-psi-nine.vercel.app/public/send.svg" width={20} alt="Send" />
-                        )}
+                        )} */}
                 </form>
             </div>
         </div>
