92ea3a99b17c72bbe3ba096f80c07ae2d9bdd837
fuad.sadigov <fuad.sadigov@aih.az>
2024-04-26 16:18:39 +0400

    add some change

--- server/index.js
+++ server/index.js
@@ -6,32 +6,30 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
 app.use(cors());
+app.use(cors({
+  origin: process.env.URL
+}));
 app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-
 app.post('/message', async (req, res) => {
   const { message } = req.body;
-  console.log("Received encrypted message:", message);
-
   try {
-    const bytes = CryptoJS.AES.decrypt(message, process.env.SECRET_KEY);
-    const decryptedRequest = bytes.toString(CryptoJS.enc.Utf8);
-    if (!decryptedRequest) {
-      throw new Error("Decryption failed. Possible encoding or key mismatch.");
-    }
-    console.log("Decrypted message:", decryptedRequest);
+    // const bytes = CryptoJS.AES.decrypt(message, process.env.SECRET_KEY);
+    // console.log(bytes);
+    // const decryptedRequest = bytes.toString(CryptoJS.enc.Utf8);
+    // if (!decryptedRequest) {
+    //   throw new Error("Decryption failed. Possible encoding or key mismatch."); 
+    // }
+    // console.log("Decrypted message:", decryptedRequest);
     const model = genAI.getGenerativeModel({ model: "models/gemini-pro"});
-    const result = await model.generateContent(decryptedRequest);
+    const result = await model.generateContent(message);
     const response = await result.response;
     const text = await response.text();
-    console.log("Response from AI model:", text);
-
     const encryptedResponse = CryptoJS.AES.encrypt(text, process.env.SECRET_KEY).toString();
-    res.json({ message: encryptedResponse });
+    res.json({ message: text});
   } catch (error) {
-    console.error("Error processing your request:", error); 
     res.status(500).send('Error processing your request');
   }
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,90 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import CryptoJS from 'crypto-js';
 import './App.css'
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [response, setResponse] = useState('');
+    const [message, setMessage] = useState('');
+    const [response, setResponse] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [results, setResults] = useState([]);
+    const resultsEndRef = useRef(null);
 
-  const sendMessage = async () => {
-    event.preventDefault();
-    try {
-      const encryptedMessage = CryptoJS.AES.encrypt(message, import.meta.env.VITE_SECRET_KEY).toString();
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ message: encryptedMessage })
-      };
-      const response = await fetch('http://localhost:5000/message', requestOptions);
-      const resultJson = await response.json();
-      const bytes = CryptoJS.AES.decrypt(resultJson.message, import.meta.env.VITE_SECRET_KEY);
-      const decryptedResponse = bytes.toString(CryptoJS.enc.Utf8);
-      setResponse(decryptedResponse);
-    } catch (error) {
-      console.error('Error:', error);
-      setResponse('Error processing request');
-    }
-  };
+    const scrollToBottom = () => {
+        resultsEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
 
-  return (
-    <div className="App">
-      <div className='container'>
-        <div className="result">
-          {<p>Hello</p>}
-          <p dangerouslySetInnerHTML={{ __html: response }}></p>
+    useEffect(() => {
+        scrollToBottom();
+    }, [results]);
+
+    const sendMessage = async () => {
+        event.preventDefault();
+        setLoading(true);
+        setResults(prevState => [...prevState, { message: message, response: false }]);
+        setMessage('')
+    
+        try {
+            const encryptedMessage = CryptoJS.AES.encrypt(JSON.stringify({ message }), import.meta.env.VITE_SECRET_KEY).toString();
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ message: message })
+            };
+    
+            const response = await fetch('https://chatbot-ecru-ten-66.vercel.app/', requestOptions);
+            const resultJson = await response.json();
+            // const bytes = CryptoJS.AES.decrypt(resultJson.message, import.meta.env.VITE_SECRET_KEY);
+            // const decryptedResponse = bytes.toString(CryptoJS.enc.Utf8);
+    
+            setResults(prevState => {
+                const newState = [...prevState];
+                newState[newState.length - 1] = { message: message, response: resultJson.message };
+                return newState;
+            });
+    
+            setResponse(resultJson.message);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error:', error);
+            setResponse('Error processing request');
+            setResults(prevState => {
+                const newState = [...prevState];
+                newState[newState.length - 1] = { message: message, response: 'Değerli hemkarım, çok özür dilerim. Yeniden gönder' };
+                return newState;
+            });
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className='container'>
+                <div className="result">
+                    {results.length > 0 ? results.map((result, index) => (
+                        <div key={index}>
+                            <div className='mes'>
+                                <div className='request'>
+                                    <div className="avatar"></div>
+                                    <div>{result.message}</div>
+                                </div>
+                                <div className='response'>
+                                    <img width={38} height={35} src="/public/sefa.jpg" alt="" />
+                                    {result.response != false ? <div className='result-text' dangerouslySetInnerHTML={{ __html: result.response }} /> : <div className='load-avatar'></div>}
+                                </div>
+                            </div>
+                        </div>
+                    )) : null}
+                    <div ref={resultsEndRef} />
+                </div>
+                <form onSubmit={sendMessage} className='form'>
+                    <input className='input' placeholder='Write something' value={message}
+                        onChange={(e) => setMessage(e.target.value)} />
+                        {message && (
+                            <img onClick={sendMessage} className='send-btn' src="/public/send.svg" width={20} alt="Send" />
+                        )}
+                </form>
+            </div>
         </div>
-        <form onSubmit={sendMessage} className='form'>
-          <input className='input' value={message} onChange={(e) => setMessage(e.target.value)} />
-          <button type='submit'>Send</button>
-        </form>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
